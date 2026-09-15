8bc966c6712be2517f373470c1bf503c79dd92bc
xiodreick <xiodreick@gmail.com>
2026-09-15 09:23:33 -0400

    cambio de sangria

--- login/usuario.py
+++ login/usuario.py
@@ -3,10 +3,10 @@ def menu_usuario():
         print("\n***** MENU DE USUARIO *****\n1. Ver información\n2. Consultar datos\n3. Realizar operación\n4. Cerrar sesión")
         try:
             opcion = int(input("Opción (1-4): "))
-            if opcion == 1: print("\nℹ️ Sistema modular en Python.")
-            elif opcion == 2: print("\n📊 No hay métricas adicionales.")
-            elif opcion == 3: print("\n⚙️ Operación estándar realizada.")
+            if opcion == 1: print("\n Sistema modular en Python.")
+            elif opcion == 2: print("\n No hay métricas adicionales.")
+            elif opcion == 3: print("\n Operación estándar realizada.")
             elif opcion == 4: print("\nCerrando sesión..."); break
-            else: print("\n❌ Opción inválida.")
+            else: print("\n Opción inválida.")
         except ValueError:
-            print("\n❌ Error: Debes ingresar un número del 1 al 4, no letras.")
+            print("\n Error: Debes ingresar un número del 1 al 4, no letras.")
