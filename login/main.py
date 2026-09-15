c74a7dbfac7353e7cdcb1dcf5bb98f163b13f227
xiodreick <xiodreick@gmail.com>
2026-09-15 09:23:55 -0400

    main

--- login/main.py
+++ login/main.py
@@ -18,11 +18,11 @@ def main():
                 tipo = funciones.login(base_usuarios)
                 if tipo == "administrador": menu_administrador(base_usuarios)
                 elif tipo == "usuario": menu_usuario()
-                else: print("\n❌ Credenciales incorrectas.")
+                else: print("\n Credenciales incorrectas.")
             elif opcion == 2:
                 print("\n¡Hasta luego!"); break
-            else: print("\n❌ Opción no válida.")
+            else: print("\n Opción no válida.")
         except ValueError:
-            print("\n❌ Error: Debes ingresar un número entero (ej. 1 o 2), no letras.")
+            print("\n Error: Debes ingresar un número entero (ej. 1 o 2), no letras.")
 
 main()
